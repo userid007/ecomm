2e78c129789cf3577402e7cdc1c7be313e19ad46
userid007 <shindeakshay7499@gmail.com>
2023-04-18 22:10:01 +0530

    express,mongodb,env setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (_req, res) => {
+  res.send("Hello there - API");
+});
+
+export default app;
