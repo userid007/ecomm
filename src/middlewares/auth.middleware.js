e29f34a4915bf2da1d91a469e91028e2eb32af01
userid007 <shindeakshay7499@gmail.com>
2023-04-22 00:15:05 +0530

    isLoggedIn, authorize middleware done

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import User from "../models/user.schema";
+import JWT from "jsonwebtoken";
+import config from "../config";
+import asyncHandler from "../service/asyncHandler";
+import CustomError from "../service/customError";
+
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies.accessToken || req.get("authorization").split(" ")[1];
+
+  if (!token) {
+    throw new CustomError("Not authorized to access this resource", 401);
+  }
+
+  try {
+    const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET);
+    req.user = await User.findById(decodedJwtPayload.sub, "name email role");
+    next();
+  } catch (error) {
+    throw new CustomError("Not authorized to access this resource", 401);
+  }
+});
+
+export const authorize = (...requiredRoles) =>
+  asyncHandler(async (req, res, next) => {
+    if (!requiredRoles.includes(req.user.role)) {
+      throw new CustomError(
+        "You are not authorized to access this resource",
+        401
+      );
+    }
+    next();
+  });
