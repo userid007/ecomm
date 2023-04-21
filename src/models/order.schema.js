d3ad2ec22840a4fcc16d7bb65e00718ce8b6f104
userid007 <shindeakshay7499@gmail.com>
2023-04-21 19:05:18 +0530

    product,order,coupon models added

---
+++ src/models/order.schema.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import OrderStatus from "../utils/orderStatus";
+
+const orderSchema = new mongoose.Schema(
+  {
+    product: {
+      type: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+          },
+          count: Number,
+          price: Number,
+        },
+      ],
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    coupon: {
+      type: String,
+    },
+    transactionId: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.ORDERED,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
---
+++ src/models/product.schema.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+      trim: true,
+      maxLength: [120, "Product name should not be more than 120 chars"],
+    },
+    price: {
+      type: Number,
+      rquired: [true, "Product price is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      trim: true,
+      maxLength: [
+        5000,
+        "Product description should not be more than 5000 chars",
+      ],
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
