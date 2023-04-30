c8ce7616077a775ab39dbfa4d113d2f3f962bc86
userid007 <shindeakshay7499@gmail.com>
2023-04-30 21:49:58 +0530

    code refactor

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,35 +1,34 @@
 import Joi from "joi";
 import validateHandler from "../service/validateHandler.js";
-const passwordPattern =
-  "^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()-__+.]){1,}).{8,}$";
-const namePattern = "^[a-zA-Z]+$";
-const signUpSchema = Joi.object({
-  name: Joi.string()
-    .max(50)
-    .pattern(new RegExp(namePattern))
-    .required()
-    .messages({
-      "string.pattern.base": "Name should only contain character",
-    }),
-  email: Joi.string().email().required(),
-  password: Joi.string()
-    .min(8)
-    .pattern(new RegExp(passwordPattern))
-    .required()
-    .messages({
-      "string.pattern.base": "Password is not strong",
-    }),
-});
 
 export const signUpValidator = (req, res, next) => {
+  const passwordPattern =
+    "^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()-__+.]){1,}).{8,}$";
+  const namePattern = "^[a-zA-Z]+$";
+  const signUpSchema = Joi.object({
+    name: Joi.string()
+      .max(50)
+      .pattern(new RegExp(namePattern))
+      .required()
+      .messages({
+        "string.pattern.base": "Name should only contain characters",
+      }),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .min(8)
+      .pattern(new RegExp(passwordPattern))
+      .required()
+      .messages({
+        "string.pattern.base": "Password is not strong",
+      }),
+  });
   validateHandler(req, res, next, signUpSchema);
 };
 
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
-
 export const loginValidator = (req, res, next) => {
+  const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
   validateHandler(req, res, next, loginSchema);
 };
--- src/validators/coupon.validator.js
+++ src/validators/coupon.validator.js
@@ -1,22 +1,20 @@
 import Joi from "joi";
 import validateHandler from "../service/validateHandler.js";
 
-const createCouponSchema = Joi.object({
-  code: Joi.string().alphanum().required(),
-  discount: Joi.number().min(0).max(100).required(),
-  active: Joi.boolean().required(),
-});
-
 export const createCouponValidator = (req, res, next) => {
+  const createCouponSchema = Joi.object({
+    code: Joi.string().alphanum().required(),
+    discount: Joi.number().min(0).max(100).required(),
+    active: Joi.boolean().required(),
+  });
   validateHandler(req, res, next, createCouponSchema);
 };
 
-const updateCouponSchema = Joi.object({
-  code: Joi.string().alphanum(),
-  discount: Joi.number().min(0).max(100),
-  active: Joi.boolean(),
-});
-
 export const updateCouponValidator = (req, res, next) => {
+  const updateCouponSchema = Joi.object({
+    code: Joi.string().alphanum(),
+    discount: Joi.number().min(0).max(100),
+    active: Joi.boolean(),
+  });
   validateHandler(req, res, next, updateCouponSchema);
 };
