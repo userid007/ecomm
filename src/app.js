f968d1fa9277b00507a5ba946bf2e24156333435
userid007 <shindeakshay7499@gmail.com>
2023-05-16 16:09:04 +0530

    swagger docs added, nodemon config added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+import fs from "fs";
 
+const swaggerDocument = YAML.parse(fs.readFileSync("./swagger.yaml", "utf8"));
 const app = express();
 
 app.use(express.json());
@@ -10,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/", routes);
 
 app.get("/", (_req, res) => {
